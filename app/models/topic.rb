c8e54e3acf1952708821ba9f3019dc160260cb07
Josh Humber <humjosh@live.com>
2017-04-14 12:04:46 +0100

    Add extra methods to Topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,9 +5,22 @@ class Topic
     attr_reader :score
     attr_accessor :content
 
-    def initialize(content)
+    def initialize(content, score = 0)
         @@instance_count += 1
         @id = @@instance_count
         @content = content
+        @score = score
+    end
+
+    def increase_score
+        @score += 1
+    end
+    
+    def decrease_score
+        @score -= 1
+    end
+
+    def <=> (other)
+        self.score <=> other.score
     end
 end
--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -1,13 +1,46 @@
 class TopicTest < ActiveSupport::TestCase
     test "topic IDs are auto-incrementing" do
         # arrange
-        topic_1 = Topic.new("content")
+        topic_1 = Topic.new('content')
 
         # act
-        topic_2 = Topic.new("more content")
+        topic_2 = Topic.new('more content')
 
         # assert
         assert_equal topic_1.id + 1, topic_2.id
+    end
+
+    test "increase score adds 1 to the score" do
+        # arrange
+        topic = Topic.new('content', 1)
+
+        # act
+        topic.increase_score()
+
+        # assert
+        assert_equal(2, topic.score)
+    end
+
+    test "decrease score removes 1 from the score" do
+        # arrange
+        topic = Topic.new('content', 20)
+
+        # act
+        topic.decrease_score()
+
+        # assert
+        assert_equal(19, topic.score)
+    end
+
+    test "negative scores are allowed" do
+        # arrange
+        topic = Topic.new('content', 0)
+
+        # act
+        topic.decrease_score()
+
+        # assert
+        assert_equal(-1, topic.score)
     end
 end
     
